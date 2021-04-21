16d01c62b82f5f7ffa253066017494059847f516
vargheseabraham <40785256+vargheseabraham@users.noreply.github.com>
2021-04-21 23:46:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,47 @@
+# Terraform state will be stored in S3
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
+  backend "s3" {
+    bucket = "terraform-bucket-dev"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
   }
 }
 
+# Use AWS Terraform provider
 provider "aws" {
-  profile = "default"
-  region  = "us-east-2"
+  region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-03d64741867e7bb94"
-  instance_type = "t2.micro"
+# Create EC2 instance
+resource "aws_instance" "default" {
+  ami                    = var.ami
+  count                  = var.instance_count
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.default.id]
+  source_dest_check      = false
+  instance_type          = var.instance_type
 
   tags = {
-    Name = "ExampleInstance"
+    Name = "terraform-default"
   }
+}
+
+# Create Security Group for EC2
+resource "aws_security_group" "default" {
+  name = "terraform-default-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
