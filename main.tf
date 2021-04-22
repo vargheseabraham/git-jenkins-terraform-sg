6a7080c62808511215ad0d2117d4c1d8ea6d72a9
vargheseabraham <40785256+vargheseabraham@users.noreply.github.com>
2021-04-22 07:20:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,14 +9,13 @@ terraform {
 
 # Use AWS Terraform provider
 provider "aws" {
+  profile = "default"
   region = "us-east-2"
 }
 
 # Create EC2 instance
 resource "aws_instance" "default" {
   ami                    = var.ami
-  count                  = var.instance_count
-  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.default.id]
   source_dest_check      = false
   instance_type          = var.instance_type
